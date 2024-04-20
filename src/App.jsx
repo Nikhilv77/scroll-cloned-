8726f1c1261205dab29683f56a4e81ff28cda997
Nikhil <Nv581339@gmail.com>
2024-04-21 04:19:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+useEffect(()=>{
+  const locomotive =()=>{
+    gsap.registerPlugin(ScrollTrigger);
+
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+  }
+  locomotive();
+},[])
+  return (
+    <>
+    <div id = "main">
+      <div className="page1"></div>
+      <div className="page2"></div>
+    </div>
+    </>
+  )
+}
+
+export default App
